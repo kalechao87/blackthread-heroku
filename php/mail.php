867c0d0a5668a439ba15a3df9c4415886626dd71
Lewy Blue <looeee@gmail.com>
2017-09-27 17:20:34 +0700

    ss

--- php/mail.php
+++ php/mail.php
@@ -25,7 +25,7 @@ Message: $message
 ";
 
 /* Send the message using mail() function */
-mail( $myemail, $subject, $emailContent, $mailheader ) or die("Sorry, the mailing server encounter and error :( \n Please email directly to looeee@gmail.com");
+mail( $myemail, $subject, $emailContent, $mailheader ) or die("Sorry, the mailing server encountered an error. Please email directly to looeee@gmail.com");
 
 /* Redirect visitor to the thank you page */
 header('Location: /thanks.html');
