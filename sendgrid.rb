de63914b61c3b8ebc5f1ab49c2427c4b173a98ac
Lewy Blue <looeee@gmail.com>
2017-11-03 09:09:45 +0700

    Added sendgrid email 4

--- sendgrid.rb
+++ sendgrid.rb
@@ -1,38 +1,30 @@
-require 'sinatra'
-require 'rack/cors'
-require 'pony'
+require 'sendgrid-ruby'
+include SendGrid
 
-use Rack::Cors do |config|
-  config.allow do |allow|
-    allow.origins '*'
-    allow.resource '/sendgrid.rb', :methods => [:post]
-  end
-end
+data = JSON.parse('{
+  "personalizations": [
+    {
+      "to": [
+        {
+          "email": "looeee@gmail.com"
+        }
+      ],
+      "subject": "Hello World from the SendGrid Ruby Library!"
+    }
+  ],
+  "from": {
+    "email": "looeee@gmail.com"
+  },
+  "content": [
+    {
+      "type": "text/plain",
+      "value": "Hello, Email!"
+    }
+  ]
+}')
 
-set :mail_options, {
-	:to => 'looeee@gmail.com',
-	:from => 'looeee@gmail.com',
-	:via => :smtp,
-	:via_options => {
-		:address => 'smtp.sendgrid.net',
-		:port => 587,
-		:domain => 'heroku.com',
-		:user_name => ENV['SENDGRID_USERNAME'],
-		:password => ENV['SENDGRID_PASSWORD'],
-		:authentication => :plain
-	}
-}
-
-post '/contact' do
-  senders_name = params[:name]
-  senders_email = params[:email]
-  message = params[:message]
-  subject = params[:subject]
-  body = "#{senders_name}, #{senders_email}\n\n#{subject}\n\n#{message}"
-
-  settings.mail_options[:body] = body
-  settings.mail_options[:subject] = "New Blackthread message"
-
-  Pony.mail(settings.mail_options)
-  redirect "/thanks"
-end
+sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+response = sg.client.mail._("send").post(request_body: data)
+puts response.status_code
+puts response.body
+puts response.headers
