3315477bfb7e2700c24582cf2bcc69bbc84372df
Lewy Blue <looeee@gmail.com>
2017-11-03 14:57:04 +0700

    Added sendgrid email

--- php/mail.php
+++ php/mail.php
@@ -9,7 +9,6 @@ $name = check_input($_POST['name'], "Please enter your name");
 $email    = check_input($_POST['email']);
 $message  = check_input($_POST['message'], "No message entered");
 $subject = check_input($_POST['subject'], "No subject entered");
-$mailheader = "From: $email \r\n";
 
 function check_input($data, $problem='')
 {
@@ -38,38 +37,32 @@ function show_error($myError)
 exit();
 }
 
-$emailContent = "Black Thread Message
-
-Name: $name
-E-mail: $email
-Message: $message
-
-";
+$emailContent = "Black Thread Message\n\nName: $name\nE-mail: $email\nMessage: $message\n";
 
 $request_body = json_decode('{
   "personalizations": [
     {
       "to": [
         {
-          "email": '.$myemail.'
+          "email": "looeee@gmail.com"
         }
       ],
       "subject": '.$subject.'
     }
   ],
   "from": {
-    "email": '.$myemail.'
+    "email": "looeee@gmail.com"
   },
   "content": [
     {
       "type": "text/plain",
-      "value": '.$message.'
+      "value": '.$emailContent.'
     }
   ]
 }');
 
 $apiKey = getenv('SENDGRID_API_KEY');
-$sg = new SendGrid($apiKey);
+$sg = new \SendGrid($apiKey);
 
 $response = $sg->client->mail()->send()->post($request_body);
 echo $response->statusCode();
