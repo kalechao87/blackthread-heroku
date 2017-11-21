c2f9a5a358876d7b2ff12b336490aca00a11db06
Lewy Blue <looeee@gmail.com>
2017-11-21 20:57:16 +0700

    updated

--- heroku.conf.php
+++ heroku.conf.php
@@ -2,7 +2,9 @@ http {
   gzip         on;
   gzip_vary    on;
   gzip_proxied any;
-  gzip_types   …;
+  gzip_comp_level 2;
+  gzip_static on;
+  gzip_types   text/plain text/css application/json application/x-javascript application/javascript text/xml application/xml application/rss+xml text/javascript image/svg+xml application/vnd.ms-fontobject application/x-font-ttf font/opentype;
 
   server {
 
